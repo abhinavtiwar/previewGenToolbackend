4b9fc1944ef3f66f3ace20f4bc5b60ce168ead88
abhinavtiwar <abhinavtiwari17200@gmail.com>
2022-07-06 18:03:30 +0530

    ..

--- vid_preview.js
+++ vid_preview.js
@@ -1,10 +1,10 @@
-const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path
+  const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path
 const ffmpeg = require('fluent-ffmpeg')
 ffmpeg.setFfmpegPath(ffmpegPath)
 
 ffmpeg('video.mp4')
   .setStartTime('00:00:03')
-  .setDuration('3')
+  .setDuration('5')
   .output('video_out.gif')
   .on('end', function(err) {
     if(!err) { console.log('conversion Done') }
