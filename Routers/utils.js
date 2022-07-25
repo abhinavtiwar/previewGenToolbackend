3de8e8abdb41a9bc958cb932ae3c5e51c1128cc3
abhinavtiwar <abhinavtiwari17200@gmail.com>
2022-07-25 15:51:19 +0530

    new

--- Routers/utils.js
+++ Routers/utils.js
@@ -1,8 +1,19 @@
-const express = require("express");
-const multer = require("multer");
-const router=express.Router();
+const multer = require('multer');
+const router = require('express').Router();
 
-app.post("/image", (req, res) => {
+
+const storage = multer.diskStorage({   
+    destination: function(req, file, cb) { 
+        cb(null, './uploads');    
+    }, 
+    filename: function (req, file, cb) { 
+        cb(null , file.originalname);   
+    }
+});
+
+const upload = multer({ storage: storage, limits : {fileSize : 1000000} }).single("file");
+
+ router.post("/uploadfile", (req, res) => {
     upload(req, res, (err) => {
      if(err) {
        res.status(400).send("Something went wrong!");
@@ -11,27 +22,4 @@ app.post("/image", (req, res) => {
    });
  });
 
-
- var storage = multer.diskStorage({   
-    destination: function(req, file, cb) { 
-       cb(null, './uploads');    
-    }, 
-    filename: function (req, file, cb) { 
-       cb(null , file.originalname);   
-    }
- });
- var upload = multer({ storage: storage,limits : {fileSize : 1000000} }).single("file");
-
-
-
-
-
-
-
-
-
-module.exports=router;
-
-
-
-
+ module.exports = router;
